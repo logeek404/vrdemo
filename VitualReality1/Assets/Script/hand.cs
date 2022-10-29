7a895b14c6ac0a38a90d404786aed693c12e5aaf
logeek404 <vxpenpom@gmail.com>
2022-10-29 23:42:54 +0800

    FINALLY BUILD THE HAND
    FINALLY BUILD THE HAND,with collider

---
+++ VitualReality1/Assets/Script/hand.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hand : MonoBehaviour
+{
+    public float speed=1.0f;
+    Animator _anim;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animatorhand();
+    }
+
+    internal void SetGrip(float readValue)
+    {
+        gripTarget = readValue;
+        Debug.Log("grip value :" + readValue);
+
+    }
+
+    internal void SetTrigger(float readValue)
+    {
+        triggerTarget = readValue;
+        Debug.Log("trigger value :" + readValue);
+    }
+
+
+    void Animatorhand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            _anim.SetFloat("Grip", gripCurrent);
+            Debug.Log("SET FLOAT:" + gripCurrent);
+
+        }
+
+
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            _anim.SetFloat("Trigger", triggerCurrent);
+            Debug.Log("SET FLOAT:" + gripCurrent);
+
+        }
+
+    }
+}
---
+++ VitualReality1/Assets/Script/handcontroller.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(ActionBasedController))]
+public class handcontroller : MonoBehaviour
+{
+    ActionBasedController controller;
+    public hand _hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<ActionBasedController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _hand.SetGrip(controller.selectAction.action.ReadValue<float>());
+        _hand.SetTrigger(controller.activateAction.action.ReadValue<float>());
+
+    }
+
+
+}
