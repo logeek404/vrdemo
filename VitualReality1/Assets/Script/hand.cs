ab6063927939162dbb989ee2b261dd9138bfeef2
logeek404 <vxpenpom@gmail.com>
2022-10-30 19:27:25 +0800

    physics-based hands
    hand with physics , ( use velocity and angularspeed)

--- VitualReality1/Assets/Script/hand.cs
+++ VitualReality1/Assets/Script/hand.cs
@@ -5,23 +5,79 @@ using UnityEngine;
 
 public class hand : MonoBehaviour
 {
-    public float speed=1.0f;
+
+
+
+    //animation
+    public float animationspeed=1.0f;
     Animator _anim;
     private float gripTarget;
     private float triggerTarget;
     private float gripCurrent;
     private float triggerCurrent;
 
+    //physics movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed= 100f;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
+
+    private Transform _followTarget;
+    private Rigidbody _body;
+
     // Start is called before the first frame update
     void Start()
     {
+        //animation
         _anim = GetComponent<Animator>();
+
+        // physics movement
+        _followTarget = followObject.transform;
+        _body = GetComponent<Rigidbody>();
+        _body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        _body.interpolation = RigidbodyInterpolation.Interpolate;
+        _body.mass = 20f;
+
+        //teleport hands
+        _body.position = _followTarget.position;
+        _body.rotation = _followTarget.rotation;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Animatorhand();
+
+        Physicsmove();
+    }
+
+    private void Physicsmove()
+    {
+        //position
+        // offset
+        var postionwithOffset = _followTarget.position + positionOffset;
+        var distance = Vector3.Distance(postionwithOffset, transform.position);
+        _body.velocity = (postionwithOffset - transform.position).normalized * followSpeed * distance;
+
+
+        //rotation
+        // offset
+        var rotationwithOffset = _followTarget.rotation * Quaternion.Euler(rotationOffset);
+
+      
+            
+        Debug.Log("rotation: " + rotationwithOffset.eulerAngles.z);
+        var q = rotationwithOffset * Quaternion.Inverse(_body.rotation);
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        if (angle > 180f)
+            angle -= 360f;
+        _body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
+
+
+
     }
 
     internal void SetGrip(float readValue)
@@ -42,7 +98,7 @@ public class hand : MonoBehaviour
     {
         if (gripCurrent != gripTarget)
         {
-            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * animationspeed);
             _anim.SetFloat("Grip", gripCurrent);
             Debug.Log("SET FLOAT:" + gripCurrent);
 
@@ -51,7 +107,7 @@ public class hand : MonoBehaviour
 
         if (triggerCurrent != triggerTarget)
         {
-            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * animationspeed);
             _anim.SetFloat("Trigger", triggerCurrent);
             Debug.Log("SET FLOAT:" + gripCurrent);
 
